68852e29a891c93c0cd7b9f321b5b81b863d2a0f
Laurensius Joshua Anrico Agustinus <joshua.anrico30@gmail.com>
2020-01-09 13:40:20 +0700

    Koneksi database dan tampilan (belum slesai)

---
+++ Joshua/read.php
@@ -0,0 +1,17 @@
+<?php 
+	// Menambahkan file koneksi.php
+	include("koneksi.php");
+
+	$sql = "SELECT * FROM tb_siswa WHERE id='1'";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	        echo "id: " . $row["id"]. " - Name: " . $row["Nama_Lengkap"] . "<br>";
+	    }
+	} else {
+	    echo "0 results";
+	}
+	$conn->close();
+ ?>
