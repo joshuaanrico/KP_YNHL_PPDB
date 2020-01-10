6efa7f8bc4d8eb071d49bfc3f99f726ceee74115
Laurensius Joshua Anrico Agustinus <joshua.anrico30@gmail.com>
2020-01-10 15:09:48 +0700

    tampilan print sudah selesai

---
+++ Joshua/test.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+<body>
+
+	<!-- header -->
+	<div class="header">
+		<table border="0" align="center">
+			<td><img src="../images/ypbu.png" width="80" height="80"></td>
+			<td><p class="noDisplay">xxxxxxxxxxxx</p></td>
+			<td>
+				<p class="pHeader1">YAYASAN NURUL HUDA LAMPUNG</p>
+				<p class="pHeader2">PONDOK PESANTREN NURUL HUDA</p>
+				<p class="pHeader3">PERGURUAN TRI SUKSES (PAUD - TK - SD - SMP - SMA - SMK)</p>
+			</td>
+		</table>
+	<hr class="garis1">
+	<p class="pHeader4">Kantor : Komplek Pondok Pesantren Nurul Huda Serbajadi II Pemanggilan Natar Lampung Selatan Telp/Fax. (0721) 780472</p>
+	<hr class="garis2">
+	</div>
+
+	<!-- content -->
+	<!-- FORMULIR PENDAFTARAN PESERTA DIDIK BARU -->
+	<div class="content">
+		<p class="pContent1">FORMULIR PENDAFTARAN PESERTA DIDIK BARU</p>
+		<i class="pContent2">&emsp;&emsp;Bagian ini diisi oleh Petugas Pendaftaran</i>
+		<table>
+			<td>
+				<p class="pContent2">&emsp;&emsp;No. Registrasi</p> 
+				<p class="pContent2">&emsp;&emsp;Nomor Induk</p>
+				<p class="pContent2">&emsp;&emsp;Sekolah Didaftar</p>
+				<p class="pContent2">&emsp;&emsp;Petugas Penerima</p>
+			</td>
+			<td><p class="noDisplay">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;xyz</p></td>
+			<td>
+				<p class="pContent2">vvv</p> 
+				<p class="pContent2">xxxx</p>
+				<p class="pContent2">yyyyy</p>
+				<p class="pContent2">zzzzzz</p>
+			</td>
+		</table>
+		<p class="noDisplay"></p>
+	</div>
+
+	<!-- A. DATA PRIBADI CALON PESERTA DIDIK -->
+	<div class="content">
+		<p class="pContent3">&nbsp;Mohon data di bawah ini dilengkapi dengan huruf kapital</p>
+		<p class="noDisplay"></p>
+		<p class="pContent4">&nbsp;A. DATA PRIBADI CALON PESERTA DIDIK</p>
+		<table>
+			<td>
+				<p class="pContent2">&emsp;1 Nama Lengkap </p>
+				<p class="pContent2">&emsp;2 NISN </p>
+				<p class="pContent2">&emsp;3 NIK </p>
+				<p class="pContent2">&emsp;4 Asal Sekolah </p>
+				<p class="pContent2">&emsp;5 No. Ujian </p>
+				<p class="pContent2">&emsp;6 SKHU </p>
+				<p class="pContent2">&emsp;7 No. Ijazah </p>
+				<p class="pContent2">&emsp;8 Jenis Kelamin </p>
+				<p class="pContent2">&emsp;9 Tempat, Tanggal Lahir </p>
+				<p class="pContent2">&emsp;10 Agama </p>
+				<p class="pContent2">&emsp;11 Suku </p>
+				<p class="pContent2">&emsp;12 Status </p>
+				<p class="pContent2">&emsp;13 Kondisi </p>
+				<p class="pContent2">&emsp;14 Keadaan Orang Tua </p>
+				<p class="pContent2">&emsp;15 Status Dalam Keluarga </p>
+				<p class="pContent2">&emsp;16 Tinggal Bersama </p>
+				<p class="pContent2">&emsp;17 Kewarganegaraan </p>
+				<p class="pContent2">&emsp;18 Anak Ke- </p>
+				<p class="pContent2">&emsp;19 Bahasa Sehari-hari </p>
+				<p class="pContent2">&emsp;20 Alamat </p>
+				<p class="pContent2">&emsp;21 Email </p>
+			</td>
+			<td><p class="noDisplay">&emsp;&emsp;xyz</p></td>
+			<td>
+				<p class="pContent2">: xyz</p> <!-- 1 -->
+				<p class="pContent2">: xyz</p> 
+				<p class="pContent2">: xyz</p> 
+				<p class="pContent2">: xyz</p> 
+				<p class="pContent2">: xyz</p> <!-- 5 -->
+				<p class="pContent2">: xyz</p> 
+				<p class="pContent2">: xyz</p> 
+				<p class="pContent2">: xyz</p> 
+				<p class="pContent2">: xyz Tgl x Bln y Thn z</p> 
+				<p class="pContent2">: xyz</p> <!-- 10 -->
+				<p class="pContent2">: xyz</p> 
+				<p class="pContent2">: x </p> 
+				<p class="pContent2">: x</p> 
+				<p class="pContent2">: x</p> 
+				<p class="pContent2">: x</p> <!-- 15 -->
+				<p class="pContent2">: x</p> 
+				<p class="pContent2">: xyz</p> 
+				<p class="pContent2">: x dari y bersaudara</p> 
+				<p class="pContent2">: xyz</p> 
+				<p class="pContent2">: Jalan xyz No x RT x RW x Desa/Kel X Kec. x Kab. x Prop x Kode Pos x</p> <!-- 20 -->
+				<p class="pContent2">: xyz</p>
+			</td>
+		</table>	
+	</div>
+
+	<!-- B. DATA ORANG TUA -->
+	<div class="content">
+		<p class="noDisplay"></p>
+		<p class="pContent4">&nbsp;B. DATA ORANG TUA</p>
+		<table>
+			<td>
+				<p class="pContent2">&emsp;1 Nama </p>
+				<p class="pContent2">&emsp;2 Pendidikan </p>
+				<p class="pContent2">&emsp;3 Pekerjaan </p>
+				<p class="pContent2">&emsp;4 Penghasilan </p>
+				<p class="pContent2">&emsp;5 Email Orang Tua </p>
+				<p class="pContent2">&emsp;6 Nama Wali </p>
+				<p class="pContent2">&emsp;7 Alamat Orang Tua / Wali </p>
+				<p class="pContent2">&emsp;8 Telepon/HP Orang Tua </p>
+				<p class="pContent2">&emsp;9 Alamat Penguhubung </p>
+				<p class="pContent2">&emsp;&emsp;Orang Tua </p>
+				<p class="pContent2">&emsp;10 Alamat Penghubung </p>
+				<p class="pContent2">&emsp;&emsp; Penanggung Jawab </p>
+			</td>
+			<td><p class="noDisplay">&emsp;&emsp;xyz</p></td>
+			<td>
+				<p class="pContent2">: Ayah x Ibu y</p> <!-- 1 -->
+				<p class="pContent2">: Ayah x Ibu y</p> 
+				<p class="pContent2">: Ayah x Ibu y</p> 
+				<p class="pContent2">: Ayah x Ibu y</p> 
+				<p class="pContent2">: xyz</p> <!-- 5 -->
+				<p class="pContent2">: xyz</p> 
+				<p class="pContent2">: xyz</p> 
+				<p class="pContent2">: xyz</p> 
+				<p class="pContent2">: Kelompok x Desa x Daerah X</p> 
+				<p class="noDisplayData"></p> 
+				<p class="pContent2">: Kelompok x Desa x Daerah x</p> <!-- 10 -->
+				<p class="noDisplayData"></p>
+			</td>
+		</table>	
+	</div>
+
+	<!-- C. RIWAYAT KESEHATAN CALON PESERTA DIDIK -->
+	<div class="content">
+		<p class="pContent4">&nbsp;C. RIWAYAT KESEHATAN CALON PESERTA DIDIK</p>
+		<table>
+			<td>
+				<p class="pContent2">&emsp;1 Golongan darah </p>
+				<p class="pContent2">&emsp;2 Berat dan Tinggi Badan </p>
+				<p class="pContent2">&emsp;3 Riwayat Penyakit </p>
+			</td>
+			<td><p class="noDisplay">&emsp;&emsp;xyz</p></td>
+			<td>
+				<p class="pContent2">: x</p> <!-- 1 -->
+				<p class="pContent2">: Berat Badan x kg Tinggi Badan y cm</p> 
+				<p class="pContent2">: z</p> 
+			</td>
+		</table>	
+	</div>
+
+	<!-- footer -->
+	<!-- Tanda tangan -->	
+	<div class="footer">
+		<p style="text-align: right;">Natar, xx xx xx 20xx</p>
+		<table align="center">
+			<td>
+				<p class="pContent2" align="center">Petugas Penerima,</p>
+				<p class="noDisplay">xyz</p>
+				<p class="noDisplay">xyz</p>
+				<p class="pContent2">___________________________</p>
+				<p class="pContent2" align="center">tanda tangan dan nama jelas</p>
+			</td>
+			<td><p class="noDisplay">&emsp;&emsp;&emsp;&emsp;xyz</p></td>
+			<td>
+				<p class="pContent2" align="center">Orang Tua/Wali,</p>
+				<p class="noDisplay">xyz</p>
+				<p class="noDisplay">xyz</p>
+				<p class="pContent2">___________________________</p>
+				<p class="pContent2" align="center">tanda tangan dan nama jelas</p>
+			</td>
+			<td><p class="noDisplay">&emsp;&emsp;&emsp;&emsp;xyz</p></td>
+			<td>
+				<p class="pContent2" align="center">Calon Peserta Didik,</p>
+				<p class="noDisplay">xyz</p>
+				<p class="noDisplay">xyz</p>
+				<p class="pContent2">___________________________</p>
+				<p class="pContent2" align="center">tanda tangan dan nama jelas</p>
+			</td>
+		</table>
+	</div>
+
+	
+</body>
+</html>
