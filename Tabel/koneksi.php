f40a5e250a0885e39367ce0c02855bc5f9601517
Laurensius Joshua Anrico Agustinus <joshua.anrico30@gmail.com>
2020-03-29 20:27:41 +0700

    Tabel sudah jadi teapi beda database

--- Tabel/koneksi.php
+++ Tabel/koneksi.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "test";
+$dbname = "test"; //nama database
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
--- Tabel/table.php
+++ Tabel/table.php
@@ -4,8 +4,24 @@
 	<title>Tabel Daftar Siswa</title>
 </head>
 <body>
-	<?php include("koneksi.php"); ?>
+	<?php
+	// Keterangan Database dan Server
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "test"; //nama database
 
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+
+	?>
+
+	<h2>Pemilihan Tingkatan</h2>
 	<form method="post" action="#">
 		<select name="dropdown">
 			<option value="sd">SD</option>
@@ -15,25 +31,177 @@
 		</select>
 		<input type="submit" value="submit">
 	</form>
-	<br>
+	<br><br>
 
 	<?php
 		switch ($_POST['dropdown'])  { 
 
 		case "sd":
-		 	echo "sd";
+		 	if (mysqli_connect_errno()){
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+
+			$result = mysqli_query($conn,"SELECT * FROM tb_siswa where tingkat = '1'"); // tingkat 1 adalah SD
+
+			echo "<table border='1'>
+			<tr>
+			<th>No</th>
+			<th>ID Card</th>
+			<th>Status Mukmin</th>
+			<th>Tingkat</th>
+			<th>NISN</th>
+			<th>Nama Lengkap</th>
+			<th>TTL</th>
+			<th>Alamat</th>
+			<th>Nama Ayah</th>
+			<th>Nama Ibu</th>
+			<th>No. Tlp</th>
+			</tr>";
+
+			while($row = mysqli_fetch_array($result)){
+				echo "<tr>";
+				echo "<td>" . $row['nomor'] . "</td>";
+				echo "<td>" . $row['id_card'] . "</td>";
+				echo "<td>" . $row['status_mukmin'] . "</td>";
+				echo "<td> SD </td>";
+				echo "<td>" . $row['nisn'] . "</td>";
+				echo "<td>" . $row['nama_lengkap'] . "</td>";
+				echo "<td>" . $row['ttl'] . "</td>";
+				echo "<td>" . $row['alamat'] . "</td>";
+				echo "<td>" . $row['nama_ayah'] . "</td>";
+				echo "<td>" . $row['nama_ibu'] . "</td>";
+				echo "<td>" . $row['no_hp_pengjawab'] . "</td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+
+			mysqli_close($conn);
 		break;
 
 		case "smp":
-		 	echo "smp";
+		 	if (mysqli_connect_errno()){
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+
+			$result = mysqli_query($conn,"SELECT * FROM tb_siswa where tingkat = '2'"); // tingkat 1 adalah SMP
+
+			echo "<table border='1'>
+			<tr>
+			<th>No</th>
+			<th>ID Card</th>
+			<th>Status Mukmin</th>
+			<th>Tingkat</th>
+			<th>NISN</th>
+			<th>Nama Lengkap</th>
+			<th>TTL</th>
+			<th>Alamat</th>
+			<th>Nama Ayah</th>
+			<th>Nama Ibu</th>
+			<th>No. Tlp</th>
+			</tr>";
+
+			while($row = mysqli_fetch_array($result)){
+				echo "<tr>";
+				echo "<td>" . $row['nomor'] . "</td>";
+				echo "<td>" . $row['id_card'] . "</td>";
+				echo "<td>" . $row['status_mukmin'] . "</td>";
+				echo "<td> SMP </td>";
+				echo "<td>" . $row['nisn'] . "</td>";
+				echo "<td>" . $row['nama_lengkap'] . "</td>";
+				echo "<td>" . $row['ttl'] . "</td>";
+				echo "<td>" . $row['alamat'] . "</td>";
+				echo "<td>" . $row['nama_ayah'] . "</td>";
+				echo "<td>" . $row['nama_ibu'] . "</td>";
+				echo "<td>" . $row['no_hp_pengjawab'] . "</td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+
+			mysqli_close($conn);
 		break;
 
 		case "sma":
-		 	echo "sma";
+		 	if (mysqli_connect_errno()){
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+
+			$result = mysqli_query($conn,"SELECT * FROM tb_siswa where tingkat = '3'"); // tingkat 1 adalah SMA
+
+			echo "<table border='1'>
+			<tr>
+			<th>No</th>
+			<th>ID Card</th>
+			<th>Status Mukmin</th>
+			<th>Tingkat</th>
+			<th>NISN</th>
+			<th>Nama Lengkap</th>
+			<th>TTL</th>
+			<th>Alamat</th>
+			<th>Nama Ayah</th>
+			<th>Nama Ibu</th>
+			<th>No. Tlp</th>
+			</tr>";
+
+			while($row = mysqli_fetch_array($result)){
+				echo "<tr>";
+				echo "<td>" . $row['nomor'] . "</td>";
+				echo "<td>" . $row['id_card'] . "</td>";
+				echo "<td>" . $row['status_mukmin'] . "</td>";
+				echo "<td> SMA </td>";
+				echo "<td>" . $row['nisn'] . "</td>";
+				echo "<td>" . $row['nama_lengkap'] . "</td>";
+				echo "<td>" . $row['ttl'] . "</td>";
+				echo "<td>" . $row['alamat'] . "</td>";
+				echo "<td>" . $row['nama_ayah'] . "</td>";
+				echo "<td>" . $row['nama_ibu'] . "</td>";
+				echo "<td>" . $row['no_hp_pengjawab'] . "</td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+
+			mysqli_close($conn);
 		break;
 		
 		case "smk":
-		 	echo "smk";
+		 	if (mysqli_connect_errno()){
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+
+			$result = mysqli_query($conn,"SELECT * FROM tb_siswa where tingkat = '4'"); // tingkat 1 adalah SMK
+
+			echo "<table border='1'>
+			<tr>
+			<th>No</th>
+			<th>ID Card</th>
+			<th>Status Mukmin</th>
+			<th>Tingkat</th>
+			<th>NISN</th>
+			<th>Nama Lengkap</th>
+			<th>TTL</th>
+			<th>Alamat</th>
+			<th>Nama Ayah</th>
+			<th>Nama Ibu</th>
+			<th>No. Tlp</th>
+			</tr>";
+
+			while($row = mysqli_fetch_array($result)){
+				echo "<tr>";
+				echo "<td>" . $row['nomor'] . "</td>";
+				echo "<td>" . $row['id_card'] . "</td>";
+				echo "<td>" . $row['status_mukmin'] . "</td>";
+				echo "<td> SMK </td>";
+				echo "<td>" . $row['nisn'] . "</td>";
+				echo "<td>" . $row['nama_lengkap'] . "</td>";
+				echo "<td>" . $row['ttl'] . "</td>";
+				echo "<td>" . $row['alamat'] . "</td>";
+				echo "<td>" . $row['nama_ayah'] . "</td>";
+				echo "<td>" . $row['nama_ibu'] . "</td>";
+				echo "<td>" . $row['no_hp_pengjawab'] . "</td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+
+			mysqli_close($conn);
 		break;
 
 		default:
